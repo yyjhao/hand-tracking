8fbd6f90a4ae38df98ea7f0f51118b10db517a25
Yujian <yyjhao@gmail.com>
2014-11-30 11:18:29 +0800

    eh

--- plot.py
+++ plot.py
@@ -5,7 +5,10 @@ import matplotlib.pyplot as plt
 f1 = "filtered"
 f2 = "bin/pllvtiptouch.txt"
 
-inds = [21, 22, 23]
+inds = [0, 1, 2]
+
+f_label = ["filtered", "unfiltered"]
+ind_label = ["x", "y", "z"]
 
 
 def transpose(mat):
@@ -24,19 +27,19 @@ def get_inds(filename, inds):
                 ]), inds)
         ]
 
-for a, b in zip(get_inds(f1, inds), get_inds(f2, inds)):
+for ind, (a, b) in enumerate(zip(get_inds(f1, inds), get_inds(f2, inds))):
     fig = plt.figure()
 
     ax = fig.add_subplot(1, 1, 0)
     ax.set_xlabel('Time')
     ax.set_ylabel('coordinate')
-    ax.plot(a[0], label=a[1])
-    ax.plot(b[0], label=b[1])
+    ax.plot(a[0], label=" ".join([f_label[0], ind_label[ind]]))
+    ax.plot(b[0], label=" ".join([f_label[1], ind_label[ind]]))
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(handles, labels)
 
     # save the image
-    # fig.savefig(a[1] + '.png')
+    fig.savefig(a[1] + '.png')
 
 # show the image
-plt.show()
+# plt.show()
